0a37539d2256dd6fd4dca14e240cee2a8603b254
Minh Nguyen <minhhien134@gmail.com>
2023-04-21 14:51:47 +0700

    config postgres db

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,15 @@
 // path: ./config/env/production/database.js
 
-const { parse } = require("pg-connection-string");
-
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-  
   return {
     connection: {
       client: 'postgres',
       connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
+        host: env("DATABASE_HOST"),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USER', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
         ssl: { rejectUnauthorized: false },
       },
       debug: false,
